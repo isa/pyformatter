abcb1b40bd34756168202cc4473efae5b466c894
Isa Goksu <isa.goksu@gmail.com>
2012-11-19 14:39:53 -0800

    Added json printer with basic options

--- formatter/ext/json.py
+++ formatter/ext/json.py
@@ -1,4 +1,17 @@
 from formatter.base import Printer
+import simplejson as json
 
 class JsonPrinter(Printer):
-   pass
+   options = {
+      'sort_keys': False,
+      'indent': 4
+   }
+
+   def __init__(self, text):
+      self.text = text
+
+   def dump(options = {}):
+      self.options.update(options)
+      code = json.loads(self.text)
+
+      return json.dumps(code, **self.options)
